d90f702f8443f261e9f5809c94a3912605bd6b26
MarcelFeo <marcelfeo29@gmail.com>
2023-12-23 12:25:32 -0300

    feat: create ParkingSpotController

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 public class ParkingSpotService {
 
     final ParkingSpotRepository parkingSpotRepository;
-    
+
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
     }
